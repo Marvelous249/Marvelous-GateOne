97f1a05816b013d5fa4e9011e9fdd701123ab379
Marvelous249 <ayomideoladimeji249@gmail.com>
2024-05-12 20:13:39 -0700

    adding new file

---
+++ GateOne/Day 6/java/AgeCalculator.java
@@ -0,0 +1,28 @@
+public class AgeCalculator {
+    public static int calculateAge(String birthDate) {
+
+        String[] parts = birthDate.split("/");
+        int day = Integer.parseInt(parts[0]);
+        int month = Integer.parseInt(parts[1]);
+        int year = Integer.parseInt(parts[2]);
+
+
+        int currentYear = 2024;
+        int currentMonth = 5;
+        int currentDay = 12;
+
+
+        int age = currentYear - year;
+
+
+        if (currentMonth < month || (currentMonth == month && currentDay < day)) {
+            age = age -1;
+
+	
+        }
+
+        System.out.print(age);
+    }
+
+
+}
