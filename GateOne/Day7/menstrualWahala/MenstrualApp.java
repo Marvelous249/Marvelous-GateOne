de873e9ae703d77178cc70d350c0977f67a421a0
Marvelous249 <ayomideoladimeji249@gmail.com>
2024-05-13 14:54:44 -0700

    adding new file

---
+++ GateOne/Day7/menstrualWahala/MenstrualApp.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+	public class MenstrualApp{
+	public static void main(String... args){
+	Scanner input = new Scanner(System.in);
+
+	System.out.println("Welcome to marvelous Menstrual app!!!");
+
+	System.out.println("Enter ur last date of ur period!!");
+	String userinput = input.nextLine();
+
+	System.out.println("Please enter today's date!!");
+	int userinput2 = input.nextInt();
+	
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+}
