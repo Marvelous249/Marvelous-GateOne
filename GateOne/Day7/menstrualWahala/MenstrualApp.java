237e41ef2cc66c2c8a2d311f14fb0444b229c9f8
Marvelous249 <ayomideoladimeji249@gmail.com>
2024-05-15 08:51:11 -0700

    adding file

--- GateOne/Day7/menstrualWahala/MenstrualApp.java
+++ GateOne/Day7/menstrualWahala/MenstrualApp.java
@@ -1,35 +1,17 @@
 import java.util.Scanner;
 
-	public class MenstrualApp{
-	public static void main(String... args){
-	Scanner input = new Scanner(System.in);
+public class MenstrualApp {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Menstrual Tracker App");
 
-	System.out.println("Welcome to marvelous Menstrual app!!!");
+    System.out.print("Enter your last period date (dd/mm/yyyy): ");
+    String lastPeriodDate = input.next();
 
-	System.out.println("Enter ur last date of ur period!!");
-	String userinput = input.nextLine();
+    System.out.print("Enter your average cycle length (in days): ");
+    int cycleLength = input.nextInt();
 
-	System.out.println("Please enter today's date!!");
-	int userinput2 = input.nextInt();
-	
-
-	
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
+    System.out.println("Your next period is expected on: " + getNextPeriodDate(lastPeriodDate, cycleLength));
+  }
+ 
 }
